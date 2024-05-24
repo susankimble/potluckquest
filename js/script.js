7ec359fced30b9bb229ed1cd247627c7e27d8d2e
susankimble <babybluemac@comcast.net>
2024-05-24 10:42:02 -0400

    working through project, assign button not showing up

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
+//console.log(assignButton)
 
 addGuestButton.addEventListener ("click",function(){
     const guest = guestInput.value;
@@ -43,3 +47,34 @@ const updateGuestCount = function(){
     }
 
 }
+const assignItems = function(){
+    const potluckItems = [
+        "potato salad",
+        "cookies",
+        "hummus",
+        "burgers",
+        "hotdogs",
+        "watermelon",
+        "buns",
+        "soda",
+        "water",
+        "popsicles",
+        "salad",
+        "chicken"
+    ];
+}
+const allGuests = document.querySelectorAll(".guest-list li")
+
+
+for (let guest of allGuests){
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems [randomPotluckIndex];
+    
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem); 
+}
+assignButton.addEventListener("click", function () {
+
+assignItems();
+   });
